5cec02610a4b0b93ffea80737412afd79e190839
luttje <2738114+luttje@users.noreply.github.com>
2026-03-24 15:03:25 +0100

    Fix typo

--- resources/views/components/content/notification.blade.php
+++ resources/views/components/content/notification.blade.php
@@ -1,12 +1,14 @@
 @props([
-    'type' => 'info',
+'type' => 'info',
 ])
-<div x-data {{ $attributes->class([
+<div x-data
+     {{
+     $attributes->class([
     'shadow rounded bg-blue-50 border-l-4 border-blue-400 p-4 mt-4',
     'bg-green-50 border-l-4 border-green-400 p-4' => $type === 'success',
     'bg-yellow-50 border-l-4 border-yellow-400 p-4' => $type === 'warning',
     'bg-red-50 border-l-4 border-red-400 p-4' => $type === 'error',
-]) }}>
+    ]) }}>
     <div class="flex justify-between">
         <div class="ml-3">
             <p class="text-sm leading-5">
@@ -14,7 +16,7 @@
             </p>
         </div>
         <div class="flex-shrink-0">
-            <x-button.close @click="$root.remove()" />
+            <x-buttons.close @click="$root.remove()" />
         </div>
     </div>
 </div>
